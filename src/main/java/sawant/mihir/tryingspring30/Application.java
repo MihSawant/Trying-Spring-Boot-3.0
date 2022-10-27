76ecf10a24ec5a0395feca514ef49bb87f4d5cf0
Mihir Sawant <mihirssawant06@gmail.com>
2022-10-27 23:55:56 +0530

    Initial Commit

---
+++ src/main/java/sawant/mihir/tryingspring30/Application.java
@@ -0,0 +1,13 @@
+package sawant.mihir.tryingspring30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/sawant/mihir/tryingspring30/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sawant.mihir.tryingspring30;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
