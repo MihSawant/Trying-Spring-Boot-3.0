f5abe6429fb9d9f4b48ae8da64db33fc894b75bf
Mihir Sawant <mihirssawant06@gmail.com>
2022-10-28 23:11:00 +0530

    Added code for percentage calculator and ProblemDetail

--- src/main/java/sawant/mihir/tryingspring30/Application.java
+++ src/main/java/sawant/mihir/tryingspring30/Application.java
@@ -2,8 +2,9 @@ package sawant.mihir.tryingspring30;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class Application {
@@ -20,4 +21,25 @@ class MyController{
 	public String helloMessage(){
 		return "Hello World !";
 	}
+
+	@GetMapping("/percentage/{marks}")
+	public String getPercentage(@PathVariable int marks){
+		double percentage = 0.0;
+		if(marks > 50) {
+			throw new IllegalArgumentException("Marks cannot be greater than 50");
+		}
+		percentage = (double) marks / 50;
+		return String.format("Percentage: %.2f",percentage * 100);
+	}
+}
+
+@ControllerAdvice
+class MarksInputExceptionHandler{
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	private ProblemDetail handleMarksGreaterThan50(){
+		return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR,
+				"Total marks must be less than 50");
+	}
+
 }
